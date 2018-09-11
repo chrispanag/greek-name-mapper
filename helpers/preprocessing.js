2d4eb9fe355677dd2911a15e2cd8f66f9f436e6a
Christos Panagiotakopoulos <chrispanag@gmail.com>
2018-09-11 23:12:29 +0300

    JSDoc

--- helpers/preprocessing.js
+++ helpers/preprocessing.js
@@ -1,5 +1,13 @@
 const { toGreek: greeklishToGreek } = require('greek-utils');
 
+/**
+ * Removes the duplicate letters from a word
+ * @example
+ * // returns 'Agelos'
+ * removeDuplicateLetters('Aggelos');
+ * @param {string} word 
+ * @returns {string}
+ */
 function removeDuplicateLetters(word) {
     const matches = word.match(/(.)\1+/g);
     let returning = word;
@@ -13,15 +21,29 @@ function removeDuplicateLetters(word) {
     return returning
 }
 
+/**
+ * Removes all vowels that don't have 1-1 translation to greeklish
+ * @param {string} word 
+ * @returns {string}
+ */
 function keepMeaningfulVowels(word) {
     return word.replace(/ι|η|ε|ο|ω|υ|Ι|Ε|Η|Ο|Ω|Υ/g, '');
 }
 
+/**
+ * Replaces 'C' with 'K' - Most times 'c' is used as 'k' in Greeklish
+ * @param {string} word 
+ * @returns {string}
+ */
 function fixC(word) {
     return word.replace('c', 'k');
 }
 
-// For Greeklish words with 'Th' as 'Θ'
+/**
+ * Replaces 'Th' with '8' - For Greeklish words with 'Th' as 'Θ'
+ * @param {string} word
+ * @returns {string} 
+ */
 function fixTh(word) {
     const position = (word.toUpperCase()).indexOf('TH');
     if (position < 0)
@@ -33,6 +55,11 @@ function fixTh(word) {
     return word;
 }
 
+/**
+ * Translates to greek from Greeklish
+ * @param {string} word - A string in Greeklish
+ * @returns {string} - A string in Greek
+ */
 function toGreek(word) {
     let fromGreeklish = greeklishToGreek(word);
     if (fromGreeklish.endsWith('σ'))
@@ -41,10 +68,20 @@ function toGreek(word) {
     return fromGreeklish;
 }
 
+/**
+ * 
+ * @param {string} char 
+ * @returns {boolean} 
+ */
 function isVowelEnglish(char) {
     return (char == 'a') || (char == 'e') || (char == 'i') || (char == 'o') || (char == 'y') || (char == 'u');
 }
 
+/**
+ * 
+ * @param {string} char 
+ * @returns {boolean}
+ */
 function isVowelGreek(char) {
     char = char.toUpperCase();
     return (char == 'Α') || (char == 'Ε') || (char == 'Η') || (char == 'Ο') || (char == 'Ι') || (char == 'Υ');
